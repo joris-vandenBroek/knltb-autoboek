b38ac74aebc78e5780846118d9929b78af4d30da
Jobber <28599777+joris-vandenBroek@users.noreply.github.com>
2026-06-08 09:01:14 +0200

    sw.js: bump naar v40

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'padel-v39';
+const CACHE = 'padel-v40';
 // index.html en manifest altijd network-first zodat updates direct zichtbaar zijn
 const NETWORK_FIRST = ['/', '/index.html', '/manifest.json'];
 const CACHE_FIRST   = ['/sw.js', '/logo.png', '/icon-192.png', '/icon-512.png'];
